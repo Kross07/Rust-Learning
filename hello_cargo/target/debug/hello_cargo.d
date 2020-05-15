de545f9cda9639fce048b19c4cc66e7f38436e42
Kross07 <65046270+Kross07@users.noreply.github.com>
2020-05-14 22:03:02 -0700

    Hello World
    Cargo practice.

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Kris\Documents\GitHub\Rust-Learning\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Kris\Documents\GitHub\Rust-Learning\hello_cargo\src\main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Kris\Documents\GitHub\Rust-Learning\hello_cargo\target\release\hello_cargo.exe: C:\Users\Kris\Documents\GitHub\Rust-Learning\hello_cargo\src\main.rs
